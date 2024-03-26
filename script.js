08fed0387c271f32e18f9e55c90c6b4997dabf9b
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2024-03-26 15:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let count = document.getElementById("counter");
+let incre = document.getElementById("incrementBtn");
+incre.addEventListener('click', function() {
+	let counterValue = parseInt(count.textContent);
+	alert('Counter Value:' + counterValue);
+	counterValue++;
+	count.textContent = counterValue;
+})
